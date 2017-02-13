3990e7a9a517f693b91bd11b3e0638b2766dc48a
smlng <s@mlng.net>
2017-02-13 12:12:13 +0100

    sock_tcp_listen: update

--- sock_tcp_listen/main.c
+++ sock_tcp_listen/main.c
@@ -15,38 +15,27 @@
 #define ENABLE_DEBUG (1)
 #include "debug.h"
 
-// the port to listen for tcp connections
+/* max num of errors, before exit */
+#define MAX_ERROR_COUNT     (100U)
+/* the port to listen for tcp connections */
 #ifndef TCP_LISTEN_PORT
 #define TCP_LISTEN_PORT     (24911U)
 #endif
-// length of buffer for tcp receive
+/* length of buffer for tcp receive */
 #define TCP_LISTEN_BUFLEN   (512U)
-// timeout in usec for tcp receive
+/* timeout in usec for tcp receive */
 #define TCP_LISTEN_TIMEOUT  (GNRC_TCP_CONNECTION_TIMEOUT_DURATION)
-// max num of errors, before exit
-#define MAX_ERROR_COUNT     (100U)
+/* sock lwip defines */
 #define TCP_QUEUE_SIZE  (1)
-
 #define TCP_NETIF           (1)
-#define TCP_ADDR6_LOCAL     { 0x2f, 0xc4, 0x11, 0x5a, 0xe6, 0x91, 0x8d, 0x5d, \
-                              0x8c, 0xd1, 0x47, 0x07, 0xb7, 0x6f, 0x9b, 0x48 }
-#define TCP_LLADDR_STR        ("fe80::cafe:cafe:cafe:42")
 
 int listen(uint16_t port)
 {
     sock_tcp_queue_t queue;
     sock_tcp_t queue_array[TCP_QUEUE_SIZE];
-    ipv6_addr_t addr;
-    if (ipv6_addr_from_str(&addr, TCP_LLADDR_STR) == NULL) {
-        puts("[ERROR] unable to parse link local address");
-        return 42;
-    }
-    sock_tcp_ep_t local;
-    memset(&local, 0, sizeof(local));
-    local.family = AF_INET6;
-    local.port = port;
-    local.netif = SOCK_ADDR_ANY_NETIF;
-    memcpy((void *)&local.addr.ipv6, (void *)&addr.u8, sizeof(addr));
+    const sock_tcp_ep_t local = {   .family = AF_INET6,
+                                    .port = port,
+                                    .netif = TCP_NETIF };
     /* open listing port */
     if (0 != sock_tcp_listen(&queue, &local, queue_array, TCP_QUEUE_SIZE,
                              SOCK_FLAGS_REUSE_EP)) {
